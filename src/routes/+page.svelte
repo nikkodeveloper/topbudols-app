650e6426bc902e4b484db73f81b5ca6c63abb732
nikkodeveloper <nikkodeveloper@gmail.com>
2023-10-02 13:25:44 +0800

    Add exclamation mark in home title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
         <div>
             <img class="w-32 mx-auto mb-10" src="{ tblogolarge }" alt="tb-logo-large">
         </div>
-        <h1 class="text-5xl font-bold">Welcome sa Top Budols</h1>
+        <h1 class="text-5xl font-bold">Welcome sa Top Budols!</h1>
         <p class="py-6">Da best choices for your next budols!</p>
         <a class="btn btn-primary" href="https://www.tiktok.com/@topbudols">Follow Us on Tiktok!</a>
       </div>
